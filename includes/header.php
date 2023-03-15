30e3400ac86a03cd9f0ba17d4f5987d1de732c87
Камиль Лугуев <kamilluguev@MacBook-Pro-Kamil.local>
2023-03-15 21:48:52 +0300

    new sayt

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<header class="header-section">
+        <div class="container-fluid">
+            <div class="inner-header">
+                <div class="logo">
+                    <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                </div>
+                <div class="nav-right">
+                    <a href="#" class="primary-btn">Make a Reservation</a>
+                </div>
+                <nav class="main-menu mobile-menu">
+                    <ul>
+                        <li class="active"><a href="./index.html">Home</a></li>
+                        <li><a href="./about-us.html">About</a></li>
+                        <li><a href="rooms.html">Rooms</a></li>
+                        <li><a href="#">Pages</a>
+                            <ul class="drop-menu">
+                                <li><a href="about-us.html">About Us</a></li>
+                                <li><a href="rooms.html">Rooms</a></li>
+                                <li><a href="services.html">Services</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="./blog.html">News</a></li>
+                        <li><a href="./contact.html">Contact</a></li>
+                    </ul>
+                </nav>
+                <div id="mobile-menu-wrap"></div>
+            </div>
+        </div>
+    </header>
